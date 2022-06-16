9ef0ea1ef54df40413cae6233d1af939607ac6c9
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-06-16 15:48:46 -0400

    Databse fails to render new posts using ThunderClient but connection to database works

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-require("dotenv").config();
 const express = require("express");
-const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
-const app = express();
 const PORT = process.env.PORT || 8080;
-const uri = process.env.MONGO_URL;
-
-// Mongoose middleware to connect to the Mongo DB
-MongoClient.connect(uri, { useUnifiedTopology: true })
-  .then(client => {
-    console.log('Connected to Database')
-    const db = client.db('')
-  })
-
-app.use(cors()); // cors package prevents CORS errors when using client side API calls
-app.use(helmet());
-app.use(morgan("common"));
-app.use(express.json()); // Enable req.body middleware
 
 // Import routes from ${./routes} directory
 const authRoutes = require("./routes/authRoute");
+
+dotenv.config();
+
+// Mongoose middleware to connect to the Mongo DB
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
+
+// app.use(cors()); // cors package prevents CORS errors when using client side API calls
+app.use(express.json()); // Enable req.body middleware
+app.use(helmet());
+app.use(morgan("common"));
 
 // Root url for respective routes
 app.use('/auth', authRoutes);
