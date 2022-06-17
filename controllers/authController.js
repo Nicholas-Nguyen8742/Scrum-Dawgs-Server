8c3b4ca7b28d5ccfef86b0d84730a2a3f8db2eb1
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-06-17 18:33:29 -0400

    Uncommented out register validation

--- controllers/authController.js
+++ controllers/authController.js
@@ -9,9 +9,9 @@ const authenticate = require('../middleware/authenticate');
 exports.register = (req, res) => {
     const { name, email, password } = req.body;
 
-    // if (!name || !email || !password) {
-    //     return res.status(400).send("Please enter the required fields.");
-    // }
+    if (!name || !email || !password) {
+        return res.status(400).send("Please enter the required fields.");
+    }
 
     const hashedPassword = bcrypt.hashSync(password, 12);
 
