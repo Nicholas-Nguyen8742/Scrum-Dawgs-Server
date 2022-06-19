524af7a70b5ef8948e026fd11085a2b3e039466a
Nicholas-Nguyen8742 <NicholasNguyen8742@gmail.com>
2022-06-19 01:08:28 -0400

    Passport intialization made on the index.js for github route

--- index.js
+++ index.js
@@ -28,49 +28,70 @@ app.use(
 /*Passport Configuration*/
 app.use(passport.initialize()); // Initialize Passport Middleware
 app.use(passport.session()); // Passport.session middleware alters the `req` object with the `user` value by converting session id from the client cookie into a deserialized user object.
+// Initialize GitHub strategy middleware
 passport.use(
-    new GitHubStrategy(
-      {
-        clientID: process.env.GITHUB_CLIENT_ID,
-        clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        callbackURL: process.env.GITHUB_CALLBACK_URL
-      },
-      (_accessToken, _refreshToken, profile, done) => {
-        // For our implementation we don't need access or refresh tokens.
-        // Profile parameter will be the profile object we get back from GitHub
-        console.log('GitHub profile:', profile);
-  
-        // First let's check if we already have this user in our DB
-        knex('users')
-          .select('id')
-          .where({ github_id: profile.id })
-          .then(user => {
-            if (user.length) {
-              // If user is found, pass the user object to serialize function
-              done(null, user[0]);
-            } else {
-              // If user isn't found, we create a record
-              knex('users')
-                .insert({
-                  github_id: profile.id,
-                  avatar_url: profile._json.avatar_url,
-                  name: profile.username
-                })
-                .then(userId => {
-                  // Pass the user object to serialize function
-                  done(null, { id: userId[0] });
-                })
-                .catch(err => {
-                  console.log('Error creating a user', err);
-                });
-            }
-          })
-          .catch(err => {
-            console.log('Error fetching a user', err);
-          });
-      }
-    )
-  );
+  new GitHubStrategy(
+    {
+      clientID: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+      callbackURL: process.env.GITHUB_CALLBACK_URL,
+    },
+    (_accessToken, _refreshToken, profile, done) => {
+      // For our implementation we don't need access or refresh tokens.
+      // Profile parameter will be the profile object we get back from GitHub
+      console.log("GitHub profile:", profile);
+
+      // First let's check if we already have this user in our DB
+      knex("users")
+        .select("id")
+        .where({ github_id: profile.id })
+        .then((user) => {
+          if (user.length) {
+            // If user is found, pass the user object to serialize function
+            done(null, user[0]);
+          } else {
+            // If user isn't found, we create a record
+            knex("users")
+              .insert({
+                github_id: profile.id,
+                avatar_url: profile._json.avatar_url,
+                name: profile.username,
+              })
+              .then((userId) => {
+                // Pass the user object to serialize function
+                done(null, { id: userId[0] });
+              })
+              .catch((err) => {
+                console.log("Error creating a user", err);
+              });
+          }
+        })
+        .catch((err) => {
+          console.log("Error fetching a user", err);
+        });
+    }
+  )
+);
+// `serializeUser` determines which data of the auth user object should be stored in the session
+// The data comes from `done` function of the strategy, result of method is attached to the session as `req.session.passport.user = 12345`
+passport.serializeUser((user, done) => {
+    console.log('serializeUser (user object):', user);
+    done(null, user.id); // Store only the user id in session
+});
+// `deserializeUser` receives a value sent from `serializeUser` `done` function
+// We can then retrieve full user information from our database using the userId
+passport.deserializeUser((userId, done) => {
+  console.log('deserializeUser (user id):', userId);
+  knex('users')  // Query user information from the database for currently authenticated user
+    .where({ id: userId })
+    .then(user => {
+      console.log('req.user:', user[0]); // Remember that knex will return an array of records, so we need to get a single record from it
+      done(null, user[0]); // The full user object will be attached to request object as `req.user`
+    })
+    .catch(err => {
+      console.log('Error finding user', err);
+    });
+});
 
 // Import routes from ${./routes} directory
 const authRoutes = require("./routes/authRoute");
